4aee999d950badbbf4f7ec6407cae2abbb81de7d
baruc sanchez <barucgs@gmail.com>
2025-06-28 21:38:16 -0600

    CHANGE about

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -13,13 +13,13 @@ const About: React.FC = () => {
       <div className="grid md:grid-cols-2 gap-12">
         <div>
           <p className="mb-8">{about.description}</p>
-          <div className="space-y-4">
-            {about.skills.map((skill, index) => (
+        </div>
+        <div>{about.skills.map((skill, index) => (
               <div key={index} className="border-b border-gray-700 py-2">
                 {skill}
               </div>
             ))}
-          </div>
+
         </div>
       </div>
     </section>
