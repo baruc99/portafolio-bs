569e4ab425c989a5115349689464955611e38fa5
baruc sanchez <barucgs@gmail.com>
2025-06-28 23:05:02 -0600

    CHANGE services

--- src/components/Services.tsx
+++ src/components/Services.tsx
@@ -10,23 +10,25 @@ const Services: React.FC = () => {
       <h2 className="text-2xl font-bold mb-10">
         {lang === 'es' ? 'Mis Servicios' : 'My Services'}
       </h2>
-      {services.map((service) => (
-        <div key={service.id} className="mb-20">
-          <div className="text-8xl font-bold mb-6">{service.number}</div>
-          <h3 className="text-3xl font-bold mb-4">{service.title}</h3>
-          <p className="mb-8 max-w-3xl">{service.description}</p>
-          <div className="space-y-2">
-            {service.details?.map((detail, index) => (
-              <div key={index} className="flex items-center space-x-2">
-                <span className="text-sm text-gray-400">
-                  ({String(index + 1).padStart(2, "0")})
-                </span>
-                <span>{detail}</span>
-              </div>
-            ))}
+      <div className='grid grid-cols-2'>
+        {services.map((service) => (
+          <div key={service.id} className="mb-20">
+            {/* <div className="text-8xl font-bold mb-6 animate-slide-in">{service.number}</div> */}
+            <h3 className="text-3xl font-bold mb-4 animate-slide-in">{service.title}</h3>
+            <p className="mb-8 max-w-3xl">{service.description}</p>
+            <div className="space-y-2">
+              {service.details?.map((detail, index) => (
+                <div key={index} className="flex items-center space-x-2 animate-slide-in">
+                  <span className="text-sm text-gray-400">
+                    ({String(index + 1).padStart(2, "0")})
+                  </span>
+                  <span>{detail}</span>
+                </div>
+              ))}
+            </div>
           </div>
-        </div>
-      ))}
+        ))}
+      </div>
     </section>
   );
 };
